68b5e007496e5d8de128084ed81036e4bcab08ca
rafaelramireza <111542208+rafaelramireza@users.noreply.github.com>
2026-08-19 08:54:24 -0600

    refactor: update certification guard to ACTIVO

--- chaincode/certificacion.go
+++ chaincode/certificacion.go
@@ -26,7 +26,7 @@ func (s *SmartContract) EmitirCertificado(
 	}
 
 	// Validar estado actual
-	if expediente.EstadoActual != EstadoEgresado {
+	if expediente.EstadoActual != EstadoActivo {
 		return ErrEstadoInvalido
 	}
 
